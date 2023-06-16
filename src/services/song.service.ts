6146410b9c1183b1dbf9f1804a015bd76b5e8695
VeraLaureano <laureanoivangerardo@gmail.com>
2023-06-16 09:56:21 -0300

    all services added

--- src/services/song.service.ts
+++ src/services/song.service.ts
@@ -1,4 +1,5 @@
 import SongModel from "../models/Song.model";
+import { Song } from "../interfaces/song.interface";
 
 /**
  * @returns all songs found
@@ -7,4 +8,45 @@ import SongModel from "../models/Song.model";
 export const findAllSongs =async () => {
     const responseSongs = await SongModel.find({})
     return responseSongs;
+}
+
+/**
+ * @param songID string
+ * @returns a soung found
+ * @type Promise
+ */
+export const findOneSong = async (songID: string) => {
+    const responseSong = await SongModel.findOne({_id: songID});
+    return responseSong;
+} 
+
+/**
+ * @param data Song
+ * @returns new song
+ * @type Promise
+ */
+export const createSong = async (data: Song) => {
+    const responseSong = await SongModel.create(data);
+    return responseSong
+}
+
+/**
+ * @param songID string
+ * @param data Song
+ * @returns update song
+ * @type Promise
+ */
+export const findAndUpdateSong = async (songID: string, data: Song) => {
+    const responseSong = await SongModel.findOneAndUpdate({_id: songID}, data, {new: true});
+    return responseSong;
+}
+
+/**
+ * @param songID string
+ * @returns delete song
+ * @type Promise
+ */
+export const findAndDeleteSong = async (songID: string) => {
+    const responseSong = await SongModel.findByIdAndDelete({_id: songID})
+    return responseSong;
 }
